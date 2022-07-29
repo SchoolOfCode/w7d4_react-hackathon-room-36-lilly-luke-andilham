a7deaa8a70ebb7ba5d55819a68fc104cae8d63e8
“lukechaplin” <lukechaplin@gmail.com>
2022-07-29 16:50:42 +0100

    added an intergration test

--- todolist/src/Components/Input/index.js
+++ todolist/src/Components/Input/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-function Input({onChange, text}) {
-return <input type= 'text' onChange={onChange} value={text}></input>
+function Input({ handleChange, text }) {
+	return <input type="text" onChange={handleChange} value={text}></input>;
 }
 
 export default Input;
